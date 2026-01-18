e5a8de579aee1c6f7076a18b8d81ebed679cb54d
MDMonirujjaman805 <monirbinkhaled1@gmail.com>
2026-01-19 00:01:17 +0600

    added dashboard button.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import styles from "@/app/ui/home.module.css";
-import { inter, lusitana, roboto } from "./ui/fonts";
+import { inter } from "./ui/fonts";
 import Image from "next/image";
 
 export default function Page() {
@@ -27,6 +26,12 @@ export default function Page() {
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
+          <Link
+            href="/dashboard"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Dashboard</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,3 @@
-import next from "eslint-config-next";
-
 export default [
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
